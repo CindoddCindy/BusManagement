17d8b7a2c6dfe482cebd022a48910a9801e3ff81
CindoddCindy <cindodcindy@gmail.com>
2021-04-15 16:19:36 +0700

    tambah semua

---
+++ pagination_and_sorting/src/main/java/paginationAndSorting/pagination_and_sorting/PaginationAndSortingApplication.java
@@ -0,0 +1,13 @@
+package paginationAndSorting.pagination_and_sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAndSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationAndSortingApplication.class, args);
+	}
+
+}
